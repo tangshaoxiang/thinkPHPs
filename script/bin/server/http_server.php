229fe7fcbb1d8b36c8a55d9b69623e642eabbd97
darian <873246925@qq.com>
2018-07-05 10:44:54 +0800

    t_up

--- script/bin/server/http_server.php
+++ script/bin/server/http_server.php
@@ -10,7 +10,7 @@ $http = new swoole_http_server("0.0.0.0", 8811);
 $http->set(
     [
         'enable_static_handler' => true,
-        'document_root' => "/home/work/hdtocs/swoole_mooc/thinkphp/public/static",
+        'document_root' => "/home/wwwroot/default/thinkPHP5/public/static",
         'worker_num' => 5,
     ]
 );
