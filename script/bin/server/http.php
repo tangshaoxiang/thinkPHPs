59a150461649a665f488829e20d321db73ebccc8
darian <873246925@qq.com>
2018-07-06 11:27:46 +0800

    t_up

--- script/bin/server/http.php
+++ script/bin/server/http.php
@@ -39,8 +39,8 @@ class Http {
         // 定义应用目录
         define('APP_PATH', __DIR__ . '/../../../application/');
         // 加载框架里面的文件
-        //require __DIR__ . '/../../../thinkphp/base.php';
-        require __DIR__ . '/../../../thinkphp/start.php';
+        require __DIR__ . '/../../../thinkphp/base.php';
+//        require __DIR__ . '/../../../thinkphp/start.php';
     }
 
     /**
