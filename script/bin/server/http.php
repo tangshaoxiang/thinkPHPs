47f2a26e8bc9c1623cda22a277435d61e376617b
darian <873246925@qq.com>
2018-07-06 11:13:51 +0800

    t_up

--- script/bin/server/http.php
+++ script/bin/server/http.php
@@ -16,7 +16,7 @@ class Http {
         $this->http->set(
             [
                 'enable_static_handler' => true,
-                'document_root' => "/home/work/hdtocs/swoole_mooc/thinkphp/public/static",
+                'document_root' => "/home/wwwroot/default/thinkPHPs/public/static",
                 'worker_num' => 4,
                 'task_worker_num' => 4,
             ]
@@ -37,10 +37,10 @@ class Http {
      */
     public function onWorkerStart($server,  $worker_id) {
         // 定义应用目录
-        define('APP_PATH', __DIR__ . '/../application/');
+        define('APP_PATH', __DIR__ . '/../../../application/');
         // 加载框架里面的文件
-        //require __DIR__ . '/../thinkphp/base.php';
-        require __DIR__ . '/../thinkphp/start.php';
+        //require __DIR__ . '/../../../thinkphp/base.php';
+        require __DIR__ . '/../../../thinkphp/start.php';
     }
 
     /**
