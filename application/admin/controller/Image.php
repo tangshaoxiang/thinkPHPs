0cf244ce00080e0a28c7c05fdd4015ae6aa04345
darian <873246925@qq.com>
2018-07-15 17:56:49 +0800

    t_up

--- application/admin/controller/Image.php
+++ application/admin/controller/Image.php
@@ -6,24 +6,7 @@ class Image
 {
 
     public function index() {
-        header('content-type:application:json;charset=utf8');
-        header('Access-Control-Allow-Origin:*');
-        header('Access-Control-Allow-Methods:POST');
-        header('Access-Control-Allow-Headers:x-requested-with,content-type');
-        $file = request()->file('file');
-         return print_r($_FILES);
-        $info = $file->move('/static/upload');
-//        print_r($file);
-        if($info) {
-            $data = [
-                'image' => config('live.host')."/upload/".$info->getSaveName(),
-            ];
-            echo 111;
-            return Util::show(config('code.success'), 'OK', $data);
-        }else {
-            echo 222;
-            return Util::show(config('code.error'), 'error');
-        }
+        print_r($_FILES);
     }
 
 }
