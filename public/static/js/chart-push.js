f51a73b663b93fbd0ec2769da3bd2178166af87b
darian <873246925@qq.com>
2018-07-16 16:01:27 +0800

    t_up

---
+++ public/static/js/chart-push.js
@@ -0,0 +1,13 @@
+$(function () {
+    $('#discuss-box').keydown(function (event) {
+        if(event.keyCode == 13){
+            var text = $(this).val();
+            var url  = "http://www.darian.shop:8811/s?index/chart/index"
+            var data = {'content':text,'game_id':1};
+            $.post(url,data,function (result) {
+                //todo
+
+            },'json')
+        }
+    })
+})
